f31507d1497aa27bd2dfe76b90f26128be84e2a1
Roberto <roberto.antoniello@studenti.unimi.it>
2022-01-18 21:58:34 +0100

    Fix tabs

--- basic/perfect_square.c
+++ basic/perfect_square.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-	int n = 1;
-	int i = 0;
-	int trovato = 0;
-	printf("Ricerco i primi 10 quadrati perfetti...\n");
-	while(i <= 10){
-		for(int j = 0; j <= n;j++){
-
-			if(	n == j * j){
-				printf("%d\n",n);
-				j++;
-				trovato = 1;
-			}
-		}
-		if(trovato == 1){
-			i++;
-			trovato = 0;
-		}
-		n++;
+    int n = 1;
+    int i = 0;
+    int trovato = 0;
+    printf("Ricerco i primi 10 quadrati perfetti...\n");
+    while(i <= 10){
+	for(int j = 0; j <= n;j++){
+            if(n == j * j){
+		printf("%d\n",n);
+		j++;
+		trovato = 1;
+	    }
 	}
-	return 0;
+	if(trovato == 1){
+	    i++;
+	    trovato = 0;
+	}
+	n++;
+    }
+    return 0;
 }
