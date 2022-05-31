b1a8b1ad7f380263ff7592dc89ae7042abb5c08f
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2022-05-31 18:14:53 +0200

    :rocket: upgrade answer

--- advanced/L07/mystery.c
+++ advanced/L07/mystery.c
@@ -1,9 +1,7 @@
 //Qual è lo scopo della funzione mystery? qual è l'obiettivo complessivo della funzione?
 //
 //risposta:
-//Data come argomento una lista L e un intero N, la funzione scorre la lista fino a che il valore contenuto nel nodo corrente 
-//è diverso dall'intero N passato come argomento.
-//A quel punto il ciclo finisce e viene restituito il nodo precedente a quello con il valore uguale a N
+//la funzione non fa altro che cercare l'elemento valure all'interno della lista e lo restituisce quando l'ha trovato.
 struct node *mystery(List l, int value){
     struct node *current = l -> head;
     struct node *temp = NULL;
